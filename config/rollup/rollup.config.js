aad9ec286a66841027e6aa31967de039850cca66
Vitaliy Isikov <visikov@gmail.com>
2020-12-20 15:39:47 -0500

    Publish cjs and es packages

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -1,15 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-
-const opts = {
-  'fluent-behavior-tree': [
-    'BehaviorTreeBuilder',
-    'BehaviorTreeStatus',
-    'StateData',
-    'BehaviorTreeNodeInterface',
-  ],
-}
 
 export default {
   input: './src/index.ts',
@@ -18,11 +7,9 @@ export default {
     format: 'es',
   },
   plugins: [
-    commonjs(opts),
     typescript({
       typescript: require('typescript'),
     }),
-    resolve(),
   ],
   treeshake: true,
 }
